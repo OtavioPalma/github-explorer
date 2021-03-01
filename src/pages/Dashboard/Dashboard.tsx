920b1bb0a02d03428946d2286316a9d39c719355
Otavio Palma <otaviopalma@gmail.com>
2021-03-01 16:44:08 -0300

    feat: github api

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,65 +1,64 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
+import { api } from '../../services/api';
+import { Repository } from '../Repository/Repository';
 import { Form, Repositories, Title } from './styles';
 
+interface Repository {
+  id: string;
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 export const Dashboard: React.FC = () => {
+  const [repository, setRepository] = useState('');
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  const handleAddRepo = (ev: FormEvent<HTMLFormElement>) => {
+    ev.preventDefault();
+
+    if (repository) {
+      api.get<Repository>(`repos/${repository}`).then(res => {
+        setRepositories([...repositories, res.data]);
+        setRepository('');
+      });
+    }
+  };
+
   return (
     <>
       <img src={logo} alt="Github Explorer Logo" />
       <Title>Explore Repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepo}>
+        <input
+          value={repository}
+          onChange={ev => setRepository(ev.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
 
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/44899968?s=460&u=e41badbdae01d70dab39670fb8033924ba59d474&v=4"
-            alt="github logo"
-          />
+        {repositories.map(repo => (
+          <a href="teste" key={repo.id}>
+            <img src={repo.owner.avatar_url} alt={repo.owner.login} />
 
-          <div>
-            <strong>OtavioPalma/github-explorer</strong>
+            <div>
+              <strong>{repo.full_name}</strong>
 
-            <p>Github Explorer GoStack Repo 🚀</p>
-          </div>
+              <p>{repo.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/44899968?s=460&u=e41badbdae01d70dab39670fb8033924ba59d474&v=4"
-            alt="github logo"
-          />
-
-          <div>
-            <strong>OtavioPalma/github-explorer</strong>
-
-            <p>Github Explorer GoStack Repo 🚀</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-
-        <a href="teste">
-          <img
-            src="https://avatars.githubusercontent.com/u/44899968?s=460&u=e41badbdae01d70dab39670fb8033924ba59d474&v=4"
-            alt="github logo"
-          />
-
-          <div>
-            <strong>OtavioPalma/github-explorer</strong>
-
-            <p>Github Explorer GoStack Repo 🚀</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
