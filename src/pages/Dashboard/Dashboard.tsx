5feeea1ad9a5d71dcb69ac0b0727628332b29ba0
Otavio Palma <otaviopalma@gmail.com>
2021-03-01 16:19:44 -0300

    feat: dashboard styles

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,66 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+import { Form, Repositories, Title } from './styles';
 
 export const Dashboard: React.FC = () => {
   return (
-    <div>
+    <>
+      <img src={logo} alt="Github Explorer Logo" />
       <Title>Explore Repositórios no Github</Title>
-    </div>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/44899968?s=460&u=e41badbdae01d70dab39670fb8033924ba59d474&v=4"
+            alt="github logo"
+          />
+
+          <div>
+            <strong>OtavioPalma/github-explorer</strong>
+
+            <p>Github Explorer GoStack Repo 🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/44899968?s=460&u=e41badbdae01d70dab39670fb8033924ba59d474&v=4"
+            alt="github logo"
+          />
+
+          <div>
+            <strong>OtavioPalma/github-explorer</strong>
+
+            <p>Github Explorer GoStack Repo 🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/44899968?s=460&u=e41badbdae01d70dab39670fb8033924ba59d474&v=4"
+            alt="github logo"
+          />
+
+          <div>
+            <strong>OtavioPalma/github-explorer</strong>
+
+            <p>Github Explorer GoStack Repo 🚀</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
   );
 };
